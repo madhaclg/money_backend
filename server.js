d27a5d87cad0997d7bec3638e373882ffd6a46a8
madhaclg <128830023+madhaclg@users.noreply.github.com>
2023-05-13 12:28:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,12 @@ app.use((req, res, next) => {
 });
 app.use(cors());
 app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   res.header("Access-Control-Allow-Headers", "Content-Type");
-  	res.setHeader("Access-Control-Allow-Origin", "*");
-		res.setHeader("Access-Control-Allow-Credentials", "true");
-		res.setHeader("Access-Control-Max-Age", "1800");
-		res.setHeader("Access-Control-Allow-Headers", "content-type");
-		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 
+	     "PUT, POST, GET, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers",
+	     "Content-Type,Origin,X-Requested-With,Accept,Authorization");
+  
   next();
 });
 
