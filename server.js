30816cd3b9bed127c8327f3aa42718c19f8aec5c
madhaclg <128830023+madhaclg@users.noreply.github.com>
2023-05-13 12:28:44 +0530

    Update server.js cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json({ extended: false }));
 connectDB();
 
@@ -13,7 +14,7 @@ app.use(express.static(path.join(__dirname, "build")))
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.use(cors());
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", 
